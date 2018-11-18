5a831bbbfa6203eb3e942e5f933b6f5a8bbc71aa
blckbrrynvdr <43585788+blckbrrynvdr@users.noreply.github.com>
2018-11-19 00:45:19 +0300

    omg

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,39 @@
-$('.burger_menu').click(function() {
-	if($(this).is('.active:not(.back)')) {
-		 $(this).addClass('back');
-	} else if ($(this).is('.back')) {
-		$(this).removeClass('back');
-	} else {
-		$(this).addClass('active');
-	}
+let menu = (function(options) {
+
+  let buttonOpen = document.querySelector(options.buttonOpen);
+  let buttonClose = document.querySelector(options.buttonClose);
+  let menu = document.querySelector(options.menu);
+  let body = document.querySelector('body');
+
+  let _toggleMenu = function(e) {
+    menu.classList.toggle('overlay--open');
+    body.classList.toggle('body-active-menu');
+  }
+
+  // let close = function(e) {
+  //   e.preventDefault();
+  //   menu.classList.remove('overlay--open');
+  //   body.classList.remove('body-active-menu');
+  // }
+  
+  let addListeners = function() {
+    buttonOpen.addEventListener('click', _toggleMenu);
+    
+    let link = document.getElementById('link');
+    
+    link.addEventListener('click', _toggleMenu);
+    buttonClose.addEventListener('click', _toggleMenu);
+  }
+
+  return {
+    open: addListeners
+    // close: close
+  };
+})({
+  buttonOpen: '#open',
+  buttonClose: '#close',
+  menu: '#overlay'
 });
+
+menu.open();
+
